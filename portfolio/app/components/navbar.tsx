0c70744c002311709b313f6d0ed9eefe22b8ade1
jamesdev4you <jamesdev4you@gmail.com>
2026-05-14 17:57:26 -0400

    navbar desktop done

--- portfolio/app/components/navbar.tsx
+++ portfolio/app/components/navbar.tsx
@@ -12,9 +12,26 @@ const NavList = [
 export default function NavBar() {
 
     const pathname = usePathname();
+    /*Finish creating the navbar by flex box three sections: Title, NavList, Button for contact. Make logo */
     return (
-        <nav className="w-full h-16 bg-background">
-            yo
+        <nav className="w-full h-20 bg-foreground flex items-center justify-evenly">
+            <div className="text-3xl font-display text-background">James Boyle</div>
+            <ul className="flex space-x-4 gap-10">
+                {NavList.map((item) => (
+                    <li key={item.href}>
+                        <Link
+                            href={item.href}
+                            className={`font-display text-3xl text-background hover:text-primary ${pathname === item.href ? "font-bold" : ""}`}
+                        >
+                            {item.name}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+            {/* From https://v1.tailwindcss.com/components/buttons */}
+            <button className="hover:cursor-pointer font-body bg-foreground hover:bg-background  text-background font-semibold hover:text-foreground py-2 px-4 border border-background  rounded">
+            Hire Me (:
+            </button>
         </nav>
     );
 
