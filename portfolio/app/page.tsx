e4d07e5ee786b427ce989e560a1579d50f197f85
jamesdev4you <jamesdev4you@gmail.com>
2026-05-16 17:54:26 -0400

    icons on hero

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,38 +1,51 @@
-"use client";
+
+
 
 {/*
   https://simpleicons.org/?q=LinkedIn for icons 
   With turbopack, configuration for social media icons were kinda weird.
   Revisit docs for a breifing in next.config.ts   
+
+  https://medium.com/@amirnasserim/how-to-use-custom-svgs-in-your-next-js-project-94d4be65eb8a
+  How to use SVGs in NextJs with turbopack.
 */}
 
 
-import GithubIcon from "@/icons/github.svg"
-import LinkedinIcon from "@/icons/linkedin.svg"
-import InstagramIcon from "@/icons/instagram.svg"
-import TwitterIcon from "@/icons/twitter.svg"
+import GithubIcon from "../public/icons/github.svg"
+import LinkedInIcon from "../public/icons/linkedin.svg"
+import InstagramIcon from "../public/icons/instagram.svg"
+import XIcon from "../public/icons/x.svg"
 
 
 const SocialMedias = [
-  { name: "Github",icon: GithubIcon, href: "https://github.com/jamesdev4you"},
-  { name: "LinkedIn",icon: LinkedinIcon, href: "https://www.linkedin.com/in/jamesboyleusf/"},
-  { name: "Instagram",icon: InstagramIcon, href: "https://www.instagram.com/jamesboyle813"},
-  { name: "X",icon: TwitterIcon, href: "https://x.com/elonmusk"},
+  { name: "Github", Icon: GithubIcon, href: "https://github.com/jamesdev4you"},
+  { name: "LinkedIn", Icon: LinkedInIcon, href: "https://www.linkedin.com/in/jamesboyleusf/"},
+  { name: "Instagram", Icon: InstagramIcon, href: "https://www.instagram.com/jamesboyle813"},
+  { name: "X", Icon: XIcon, href: "https://x.com/elonmusk"},
 ];
 
 export default function Home() {
+  console.log("GithubIcon:", GithubIcon, typeof GithubIcon);
   return (
     <div className="flex flex-col w-full h-full bg-foreground">
 
     {/* Hero section */}
     <div className="flex items-center justify-center bg-foreground w-full h-180">
       {/* Hero Content */}
-      <div className="flex items-center justify-center w-2/3 h-full bg-red-500"> 
+      <div className="flex items-center justify-center w-2/3 h-full "> 
         {/* Hero Text + Links*/}
-        <div className="flex flex-col items-start justify-center w-1/2 h-full bg-green-500">
+        <div className="flex flex-col items-start justify-center w-1/2 h-full ">
           <h1 className="text-6xl font-display text-background">A Groovy Developer Fitting Any Vibe.</h1>
-          <div className="w-1/3">
-          
+          <div className="flex items-center justify-start space-x-4 mt-6">
+            {SocialMedias.map(({ name, Icon, href }) => (
+              <a key={name} href={href} target="_blank" rel="noopener noreferrer">
+                <Icon
+                  width={50}
+                  height={50}
+                  className=" text-background p-2 border-2 hover:text-foreground hover:bg-background hover:border-background rounded-2xl"
+                />
+              </a>
+            ))}
           </div>
         </div>
       </div>
--- portfolio/next.config.ts
+++ portfolio/next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+{/* Revisit https://nextjs.org/docs/app/api-reference/config/next-config-js/turbopack#configuring-webpack-loaders */}
+
 const nextConfig: NextConfig = {
+  /*config*/
   turbopack: {
     rules: {
-      '*.svg': {
-        loaders: ['@svgr/webpack'],
-        as: '*.js',
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
       },
     },
   },
