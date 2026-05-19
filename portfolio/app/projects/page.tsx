4ee6f700c7e8b915ba2a917385bd07f5a6d1ae74
jamesdev4you <jamesdev4you@gmail.com>
2026-05-19 13:00:31 -0400

    portfolio starting

---
+++ portfolio/app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects(){
+    return (
+        <div className=" flex justify-center items-center w-full h-full bg-foreground ">
+            <div className="mt-50 flex items-start justify-center bg-foreground w-11/12 h-screen">
+                <h1 className="text-8xl font-display text-background">Some Gnarly Projects</h1> 
+            </div>
+        </div>
+    )
+}
